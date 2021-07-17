e000b11f28964616ddd7f4076d68d6036b444357
kong430 <alslfkd0430@gmail.com>
2021-07-17 16:02:25 +0900

    merge1

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { authService, dbService } from "fbase";
 import { useHistory } from "react-router-dom";
+import Calendardraw from "components/Calendar";
 
 export default ({ userObj, refreshUser }) => {
   const onLogoutClick = () => {
@@ -9,7 +10,11 @@ export default ({ userObj, refreshUser }) => {
 
   return (
     <>
+      <div>
       <button onClick={onLogoutClick} className = "LogOut">Log Out</button>
+      </div>
+      <Calendardraw/>
+      
     </>
   );
 };
